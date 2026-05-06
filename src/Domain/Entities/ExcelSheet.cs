d7352a7516fbe39e1d69f01b8def44f69fa3aa62
Micael Souza <micael.ns@hotmail.com>
2026-05-06 19:10:09 -0300

    chore: Classes de Excel tratada domo domínio e infra.

--- src/Domain/Entities/ExcelSheet.cs
+++ src/Domain/Entities/ExcelSheet.cs
@@ -1,4 +1,4 @@
-﻿namespace Application.Contracts.Excel;
+﻿namespace Domain.Entities;
 
 public class ExcelSheet
 {
--- src/Infra/External/Excel/ExcelService.cs
+++ src/Infra/External/Excel/ExcelService.cs
@@ -1,49 +1,24 @@
 ﻿using Application.Contracts;
-using Application.Contracts.Excel;
 using ClosedXML.Excel;
-using DocumentFormat.OpenXml.Spreadsheet;
+using Domain.Entities;
 
 namespace Infra.External.Excel;
 
 public class ExcelService : IExcelService
 {
-    public byte[] Generate()
-    {
-        using var workbook = new XLWorkbook();
-        var worksheet = workbook.Worksheets.Add("Relatório");
-
-        worksheet.Cell(1, 1).Value = "VehicleId";
-        worksheet.Cell(1, 2).Value = "TravelId";
-        worksheet.Cell(1, 3).Value = "Data";
-
-        worksheet.Cell(2, 1).Value = "Coluna1";
-        worksheet.Cell(2, 2).Value = "Coluna2";
-        worksheet.Cell(2, 3).Value = DateTime.Now;
-
-        using var stream = new MemoryStream();
-        workbook.SaveAs(stream);
-
-        return stream.ToArray();
-    }
-
     public byte[] Generate(ExcelDocument document)
     {
         using var workbook = new XLWorkbook();
         foreach (var sheet in document.Sheets)
         {
             var ws = workbook.Worksheets.Add(sheet.Name);
-            PrepareHeader(ws, sheet);
+            ws.Cell(1, 1).InsertData(new[] { sheet.Columns });
             PrepareBody(ws, sheet);
         }
         using var stream = new MemoryStream();
         workbook.SaveAs(stream);
 
         return stream.ToArray();
-    }
-
-    private static void PrepareHeader(IXLWorksheet ws, ExcelSheet sheet)
-    {
-        ws.Cell(1, 1).InsertData(new[] { sheet.Columns });    
     }
 
     private static void PrepareBody(IXLWorksheet ws, ExcelSheet sheet)
